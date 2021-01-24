4adf4a9b2fa6ce316745c0a85d9a4218c92c14d2
Nouani <nouanigabriel@hotmail.com>
2021-01-24 10:22:39 -0300

    :sparkles: FEATURE: Stiliation of repository page
    Signed-off-by: Nouani <nouanigabriel@hotmail.com>

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,14 +1,64 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Header, RepositoryInfo, Issues } from './styles';
 
 interface RepositoryParams {
     repository: string;
 }
 
 const Repository: React.FC = () => {
-    const { params } = useRouteMatch<RepositoryParams>();
+    // const { params } = useRouteMatch<RepositoryParams>();
 
-    return <h1>Repository: {params.repository}</h1>;
+    return (
+        <>
+            <Header>
+                <img src={logoImg} alt="GitHub Explorer" />
+                <Link to="/">
+                    <FiChevronLeft size={16} />
+                    Voltar
+                </Link>
+            </Header>
+            <RepositoryInfo>
+                <header>
+                    <img
+                        src="https://avatars.githubusercontent.com/u/49238044?s=460&u=b4bfbb87c210a3e38c20fbe1b9a4919fad8395a5&v=4"
+                        alt="AA"
+                    />
+                    <div>
+                        <strong>rocketseat/unform</strong>
+                        <p>descrição do repo</p>
+                    </div>
+                </header>
+                <ul>
+                    <li>
+                        <strong>1808</strong>
+                        <span>Stars</span>
+                    </li>
+                    <li>
+                        <strong>48</strong>
+                        <span>Forks</span>
+                    </li>
+                    <li>
+                        <strong>67</strong>
+                        <span>Issues abertas</span>
+                    </li>
+                </ul>
+            </RepositoryInfo>
+            <Issues>
+                <Link to="asdsada">
+                    <div>
+                        <strong>asdad</strong>
+                        <p>adsad</p>
+                    </div>
+                    <FiChevronRight size={20} />
+                </Link>
+            </Issues>
+        </>
+    );
 };
 
 export default Repository;
