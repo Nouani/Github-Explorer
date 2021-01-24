0c104ee0a3e6cca26734751ffbacbaefbf9e5749
Nouani <nouanigabriel@hotmail.com>
2021-01-24 08:26:02 -0300

    :sparkles: FEATURE: Stilization of initial page
    Signed-off-by: Nouani <nouanigabriel@hotmail.com>

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,67 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-    return <Title>Dashboard</Title>;
+    return (
+        <>
+            <img src={logoImg} alt="GitHub Explorer" />
+            <Title>Explore repositórios no GitHub</Title>
+
+            <Form>
+                <input type="text" placeholder="Digite o nome do repositório" />
+                <button type="submit">Pesquisar</button>
+            </Form>
+
+            <Repositories>
+                <a href="/">
+                    <img
+                        src="https://avatars.githubusercontent.com/u/49238044?s=460&u=b4bfbb87c210a3e38c20fbe1b9a4919fad8395a5&v=4"
+                        alt="t"
+                    />
+                    <div>
+                        <strong>rocketseat/unform</strong>
+                        <p>
+                            Easy peasy highly scalable ReactJS & React Native
+                            forms!
+                        </p>
+                    </div>
+                    <FiChevronRight size={20} />
+                </a>
+                <a href="/">
+                    <img
+                        src="https://avatars.githubusercontent.com/u/49238044?s=460&u=b4bfbb87c210a3e38c20fbe1b9a4919fad8395a5&v=4"
+                        alt="t"
+                    />
+                    <div>
+                        <strong>rocketseat/unform</strong>
+                        <p>
+                            Easy peasy highly scalable ReactJS & React Native
+                            forms!
+                        </p>
+                    </div>
+                    <FiChevronRight size={20} />
+                </a>
+                <a href="/">
+                    <img
+                        src="https://avatars.githubusercontent.com/u/49238044?s=460&u=b4bfbb87c210a3e38c20fbe1b9a4919fad8395a5&v=4"
+                        alt="t"
+                    />
+                    <div>
+                        <strong>rocketseat/unform</strong>
+                        <p>
+                            Easy peasy highly scalable ReactJS & React Native
+                            forms!
+                        </p>
+                    </div>
+                    <FiChevronRight size={20} />
+                </a>
+            </Repositories>
+        </>
+    );
 };
 
 export default Dashboard;
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,6 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
-import githubBackground from '../assets/logo.svg';
+import githubBackground from '../assets/github-logo.svg';
 
 export default createGlobalStyle`
     * {
